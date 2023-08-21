de58149d30b4f4535ef9dbd8e475818099704a17
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2023-08-21 21:29:15 +0500

    commit

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,64 +1,75 @@
 import React, { useState } from "react";
-import "./Login.css";
 import { useMutation } from "react-query";
-import { loginUser } from "../../api";
+import "./Login.css";
+import { registerUser } from "../../api";
+import SmsCode from "../SmsCode/SmsCode";
 import { useTranslation } from "react-i18next";
 
 function Login({ handleClose }) {
   const [formData, setFormData] = useState({
-    phoneNumber: "998",
-    password: "",
+    phoneNumber: "",
   });
   const [code, setCode] = useState(false);
-  const mutation = useMutation((userData) =>
-    loginUser(userData, setCode, handleClose)
-  );
+  const mutation = useMutation((userData) => registerUser(userData, setCode));
   const { t } = useTranslation();
-
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormData((prevData) => ({ ...prevData, [name]: value }));
+    if (/^[0-9+-]*$/.test(value)) {
+      // 998 bilan boshlanganligini tekshiramiz
+      if (value.startsWith("998")) {
+        setFormData((prevFormData) => ({
+          ...prevFormData,
+          [name]: value,
+        }));
+      } else {
+        setFormData((prevFormData) => ({
+          ...prevFormData,
+          [name]: "998" + value, // 998 bilan boshlash
+        }));
+      }
+    }
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     mutation.mutate(formData);
   };
+
   return (
-    <div>
-      <h3 className="register-name">{t("hello30")}</h3>
-      <form onSubmit={handleSubmit} action="" className="register-form">
-        <label htmlFor="">
-          {t("hello45")}
-          <input
-            id="phoneNumber"
-            name="phoneNumber"
-            value={formData.phoneNumber}
-            onChange={handleChange}
-            type="number"
-            maxLength="14"
-            className="register-input"
-          />
-        </label>
-        <label htmlFor="">
-          {t("hello46")}
-          <input
-            id="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            type="password"
-            className="register-input"
-          />
-        </label>
-        <a className="login-password" href="#">
-          {t("hello47")}
-        </a>
-        <button type="submit" className="form-button">
-          {t("hello15")}
-        </button>
-      </form>
-    </div>
+    <>
+      {code === true ? (
+        <SmsCode handleClose={handleClose} phoneNumber={formData.phoneNumber} />
+      ) : (
+        <div>
+          <h3 className="register-name">{t("hello14")}</h3>
+          <form onSubmit={handleSubmit} action="" className="register-form">
+            <label htmlFor="phoneNumber">
+              {t("hello45")}
+              <input
+                type="tell"
+                id="phoneNumber"
+                name="phoneNumber"
+                value={formData.phoneNumber}
+                onChange={handleChange}
+                className="register-input"
+                maxLength="13"
+                required
+                placeholder="998"
+                pattern="^[0-9+-]*$"
+              />
+            </label>
+            <p style={{ width: "450px" }}>{t("hello69")}</p>
+            <div className="register-label">
+              <input required type="checkbox" />
+              <p>{t("hello70")}</p>
+            </div>
+            <button type="submit" className="form-button">
+              {t("hello14")}
+            </button>
+          </form>
+        </div>
+      )}
+    </>
   );
 }
 
